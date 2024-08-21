5f38f3dc82318d49d2c6b0a6095927bdec35a09b
zbarzanooni@yahoo.com <zbarzanooni@yahoo.com>
2024-08-21 05:13:34 -0700

    add datatable

--- app/Http/Controllers/DataTablesController.php
+++ app/Http/Controllers/DataTablesController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 
 class DataTablesController extends Controller
 {
-    public function data()
+    public function data(Request $request)
     {
-        $model = Listing::get();
-        return Datatables::of($model)->make(true);
+        if ($request->ajax()) {
+            $data = Listing::all();
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('deadline', function ($data){
+                    $time = new Carbon($data['deadline']);
+                    return $time->diff(Carbon::now())->format('%y سال')  ;
+                })
+                ->addColumn('action', function($row){
+                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
 //        $data = Listing::all();
 //        return DataTables::of($data)
 //            ->addIndexColumn()
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -17,7 +17,7 @@
                             <th>سن</th>
                             <th>ادرس</th>
                             <th>حقوق</th>
-                            <th width="105px">Action</th>
+                            <th width="105px" name="action">Action</th>
                         </tr>
                         </thead>
 
@@ -27,29 +27,4 @@
         </div>
     </div>
 @endsection
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
-<script type="text/javascript">
-    $(document).ready(function() {
 
-        $('#dataTable').DataTable({
-
-            processing: true,
-
-            serverSide: true,
-
-            ajax: "{{ route('data.tables.data') }}",
-
-            columns: [
-                { data: 'title', name: 'title' },
-                { data: 'rolse', name: 'rolse' },
-                // Define more columns as per your table structure
-
-            ]
-
-        });
-    });
-</script>
