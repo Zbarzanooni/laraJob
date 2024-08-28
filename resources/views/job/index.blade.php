7db13fc831215eaebcad309b386e8926c24fc183
zbarzanooni@yahoo.com <zbarzanooni@yahoo.com>
2024-08-28 09:26:47 -0700

    set direction  right to left

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin.main')
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="col-md-12">
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-table me-1"></i>
