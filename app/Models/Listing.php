0917350b6528d74034020cf466c1ae278c039570
zbarzanooni@yahoo.com <zbarzanooni@yahoo.com>
2024-09-14 04:45:31 -0700

    add new table for relation of listing and user

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -12,4 +12,10 @@ class Listing extends Model
         [
         'id','title','description','salary','rolse','address','deadline','image','job_type','user_id'
         ];
+
+    public function users(){
+        return $this->belongsToMany(User::class,'listing_user','listing_id','user_id')
+            ->withPivot('interview')
+            ->withTimestamps();
+    }
 }
