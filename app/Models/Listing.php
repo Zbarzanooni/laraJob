b7f5ed5b406f67444e3fab8fbc6c33dafc510d09
zbarzanooni@yahoo.com <zbarzanooni@yahoo.com>
2024-09-14 12:58:00 -0700

    add slug lib

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -3,11 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\HasSlug;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\SlugOptions;
 
 class Listing extends Model
 {
-    use HasFactory;
+    use HasSlug;
+    use HasFactory ;
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
     protected $fillable =
         [
         'id','title','description','salary','rolse','address','deadline','image','job_type','user_id'
